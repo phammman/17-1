4be2c1a13b13456ff349ac9e868a13d30904faf1
phammman <manpham1907@gmail.com>
2024-01-22 10:16:35 +0700

    baitapthuchanhjava

---
+++ baithuchanhjava/ptb1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GiaiPhuongTrinhBac1 {
+
+    public static void main(String[] args) {
+        // Nhap vao he so a, b
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap he so a: ");
+        int a = scanner.nextInt();
+        System.out.println("Nhap he so b: ");
+        int b = scanner.nextInt();
+
+        // Giai phuong trinh
+        if (a != 0) {
+            int x = -b / a;
+            System.out.println("Nghiem cua phuong trinh la: x = " + x);
+        } else {
+            if (b == 0) {
+                System.out.println("Phuong trinh co vo so nghiem.");
+            } else {
+                System.out.println("Phuong trinh vo nghiem.");
+            }
+        }
+    }
+}
---
+++ baithuchanhjava/ptb2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GiaiPhuongTrinhBac2 {
+
+    public static void main(String[] args) {
+        // Nhập vào hệ số a, b, c
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập hệ số a: ");
+        int a = scanner.nextInt();
+        System.out.println("Nhập hệ số b: ");
+        int b = scanner.nextInt();
+        System.out.println("Nhập hệ số c: ");
+        int c = scanner.nextInt();
+
+        // Tính delta
+        int delta = b * b - 4 * a * c;
+
+        // Giải phương trình
+        if (delta > 0) {
+            int x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            int x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Nghiệm của phương trình là: x1 = " + x1 + ", x2 = " + x2);
+        } else if (delta == 0) {
+            int x = -b / (2 * a);
+            System.out.println("Nghiệm của phương trình là: x = " + x);
+        } else {
+            System.out.println("Phương trình vô nghiệm.");
+        }
+    }
+}
