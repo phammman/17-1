0f1235e96810d1c7036336d65ae9d9d77b45a8b0
phammman <manpham1907@gmail.com>
2024-05-22 13:58:53 +0700

    update

--- lab2/chuong3/App.java
+++ lab2/chuong3/App.java
@@ -2,6 +2,6 @@ package lab2.chuong3;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        System.out.println("Hello cac ban!");
     }
 }
